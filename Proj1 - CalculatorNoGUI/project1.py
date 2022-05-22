703efcc73c3f12e2d9e1226977647b08ec4dcf7a
ASalazar46 <andysal737@gmail.com>
2022-05-22 13:22:27 -0700

    Established working code

--- Proj1 - CalculatorNoGUI/project1.py
+++ Proj1 - CalculatorNoGUI/project1.py
@@ -14,15 +14,47 @@ print('''\
     ** --> exponential 
 ''')
 print('''\
-For me to work properly, please type your input in this format:
+For this calculator to work properly, please type in this format:
 <command> <value1> <value2>
 ''')
 
 run_flag = 1
 while run_flag:
     # Wait for user input
-    x = input("--> ")
-    print('Your input is: ', x)
-# Check for correct syntax input
-
-# Check for undefined command input
+    ui = input("--> ")
+    
+    # Extract arguments into a list
+    uiArgs = ui.split(' ')
+    command, v1, v2 = uiArgs
+    
+    if command != '-q':
+        # Convert values from strings into their respective types
+        if '.' in v1:
+            v1 = float(v1)
+        else:
+            v1 = int(v1)
+        if '.' in v2:
+            v2 = float(v2)
+        else:
+            v2 = int(v2)
+            
+        # Parse command
+        result = 0
+        match command:
+            case '+':
+                result = v1 + v2
+            case '-':
+                result = v1 - v2
+            case '*':
+                result = v1 * v2
+            case '/':
+                result = v1 / v2
+            case '**':
+                result = v1 ** v2
+            case _:
+                result = "Unknown command. Try again."
+        # Print result
+        print(result)
+    elif command == '-q':
+        run_flag = 0
+        print('Exiting Calculator...')
