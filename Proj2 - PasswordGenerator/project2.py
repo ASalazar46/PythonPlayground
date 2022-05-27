3a20b00f854b862321cb51d43d3b14cc00f162c1
ASalazar46 <andysal737@gmail.com>
2022-05-27 00:30:12 -0700

    Main python file

---
+++ Proj2 - PasswordGenerator/project2.py
@@ -0,0 +1,41 @@
+import argparse
+import secrets
+import string
+
+# Handle the generation of the password given the arguments from
+# the command line
+def GenPass(length, setFlags):
+    symbolSet = ''
+    for flag in setFlags:
+        match flag:
+            case 'l':
+                symbolSet += string.ascii_lowercase
+            case 'u':
+                symbolSet += string.ascii_uppercase
+            case 'p':
+                symbolSet += string.punctuation
+            case 'n':
+                symbolSet += string.digits
+            case _:
+                error_msg = 'Error: cannot parse argument `symbols`.\nTry again, or type \'py project2.py -h\' for help.'
+                return error_msg
+    return ''.join(secrets.choice(symbolSet) for char in range(length))
+
+# Process arguments from command line
+# First step: create a parser
+parserObj  = argparse.ArgumentParser(description='Generate a password.')
+
+# Second step: fill in argument information
+# One for `passLength` ...
+parserObj.add_argument('passLength', type=int, help='Desired password length')
+
+# Another for `symbols` ...
+parserObj.add_argument('symbols', help='Inclusion of symbol sets within password.')
+
+# Finally, acquire the arguments from the command line
+clArgs = parserObj.parse_args()
+#print(clArgs.passLength, clArgs.symbols)
+
+# Generate the password using the arguments
+print(GenPass(clArgs.passLength, clArgs.symbols))
+
