3e7cfc4764ffa7fc85afb0f98f1cd8f8feb078a6
ASalazar46 <andysal737@gmail.com>
2022-06-01 16:48:30 -0700

    Project files modified

--- Proj3 - Converter/project3.py
+++ Proj3 - Converter/project3.py
@@ -1,13 +1,10 @@
 import defs
 
 clArgs = defs.get_arguments()
-#print('Arguments received: ', clArgs)
 
-result = defs.unit_convert(
+print(defs.unit_convert(
     clArgs.type, 
     clArgs.number, 
     clArgs.unit, 
     clArgs.convertUnit
-)
-
-print(result, clArgs.convertUnit)
+), clArgs.convertUnit)
