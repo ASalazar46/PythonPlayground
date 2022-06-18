f7c67b428f35711e1838a3e8f6cfdc046c31d2a2
ASalazar46 <andysal737@gmail.com>
2022-06-17 22:45:43 -0700

    Tested methods for linked list

--- Proj5 - DataStructures/linkedlist.py
+++ Proj5 - DataStructures/linkedlist.py
@@ -9,44 +9,55 @@ class LinkedList:
         self._headPtr = None
     
     def printList(self):
-        print('Printing list...')
         temp = self._headPtr
         while(temp):
             print(temp._data)
             temp = temp._nextPtr
-        print('Print complete.')
 
     def append(self, node):
         print('Appending to list...')
         if isinstance(node, LL_Node) == False:
             print('Argument not of the class LL_Node.')
         else:
-            temp = self._headPtr
-            appended = False
-            while(appended != True):
-                if temp._nextPtr == None:
-                    temp._nextPtr = node
-                    appended = True
-                    print('Append complete.')
-                else:
-                    temp = temp._nextPtr
+            if self._headPtr == None:
+                self._headPtr = node
+                print('Append complete.')
+            else:
+                temp = self._headPtr
+                appended = False
+                while(appended != True):
+                    if temp._nextPtr == None:
+                        temp._nextPtr = node
+                        appended = True
+                        print('Append complete.')
+                    else:
+                        temp = temp._nextPtr
 
     def pop(self):
         print('Popping list tail...')
-        newTail = self._headPtr
-        deleteMe = self._headPtr._nextPtr
-    
-        deleted = False
-        while(deleted != True):
-            if deleteMe._nextPtr == None:
-                newTail._nextPtr = None
-                del deleteMe
-                del newTail
-                deleted = True
-                print('Popped list tail.')
-            else:
-                newTail = deleteMe
-                deleteMe = deleteMe._nextPtr
+        if self._headPtr == None:
+            print('Nothing to pop. Linked list is empty.')
+        else:
+            newTail = self._headPtr
+            deleteMe = self._headPtr._nextPtr
+        
+            deleted = False
+            while(deleted != True):
+                if newTail._nextPtr == None:
+                    self._headPtr = None
+                    del newTail
+                    del deleteMe
+                    deleted = True
+                    print('Popped list tail.')
+                elif deleteMe._nextPtr == None:
+                    newTail._nextPtr = None
+                    del deleteMe
+                    del newTail
+                    deleted = True
+                    print('Popped list tail.')
+                else:
+                    newTail = deleteMe
+                    deleteMe = deleteMe._nextPtr
 
     def appendHead(self, node):
         print('Appending to list head...')
@@ -59,39 +70,80 @@ class LinkedList:
 
     def popHead(self):
         print('Popping list head...')
-        temp = self._headPtr
-        self._headPtr = self._headPtr._nextPtr
-        temp._nextPtr = None
-        del temp
-        print('Popped list head.')
+        if self._headPtr == None:
+            print('Nothing to pop. Linked list is empty.')
+        else:
+            temp = self._headPtr
+            self._headPtr = self._headPtr._nextPtr
+            temp._nextPtr = None
+            del temp
+            print('Popped list head.')
         
 class LL_Node:
     def __init__(self, data):
         self._data = data
         self._nextPtr = None
 
-linkedList = LinkedList()
 
-firstNode = LL_Node('First')
-secondNode = LL_Node('Second')
-thirdNode = LL_Node('Third')
+def testAppend():
+    print('-'*60)
+    print('Testing LinkedList.append():')
+    testLL = LinkedList()
+    testLL.append(LL_Node('First'))
+    testLL.printList()
+    testLL.append(LL_Node('Second'))
+    testLL.printList()
+    testLL.append(LL_Node('Third'))
+    testLL.printList()
+    print('-'*60)
 
-linkedList._headPtr = firstNode
-firstNode._nextPtr = secondNode
-secondNode._nextPtr = thirdNode
+def testPop():
+    print('-'*60)
+    print('Testing LinkedList.pop():')
+    testLL = LinkedList()
+    testLL.append(LL_Node('First'))
+    testLL.append(LL_Node('Second'))
+    testLL.append(LL_Node('Third'))
+    testLL.printList()
+    testLL.pop()
+    testLL.printList()
+    testLL.pop()
+    testLL.printList()
+    testLL.pop()
+    testLL.printList()
+    testLL.pop()
+    print('-'*60)
 
-linkedList.printList()
+def testAppendHead():
+    print('-'*60)
+    print('Testing LinkedList.appendHead():')
+    testLL = LinkedList()
+    testLL.appendHead(LL_Node('First'))
+    testLL.printList()
+    testLL.appendHead(LL_Node('Second'))
+    testLL.printList()
+    testLL.appendHead(LL_Node('Third'))
+    testLL.printList()
+    print('-'*60)
 
-linkedList.append(LL_Node('Fourth'))
-linkedList.printList()
+def testPopHead():
+    print('-'*60)
+    print('Testing LinkedList.appendHead():')
+    testLL = LinkedList()
+    testLL.appendHead(LL_Node('First'))
+    testLL.appendHead(LL_Node('Second'))
+    testLL.appendHead(LL_Node('Third'))
+    testLL.printList()
+    testLL.popHead()
+    testLL.printList()
+    testLL.popHead()
+    testLL.printList()
+    testLL.popHead()
+    testLL.printList()
+    testLL.popHead()
+    print('-'*60)
 
-linkedList.append('asdf')
-
-linkedList.popHead()
-linkedList.printList()
-
-linkedList.appendHead(LL_Node('Zeroth'))
-linkedList.printList()
-
-linkedList.pop()
-linkedList.printList()
+testAppend()
+testPop()
+testAppendHead()
+testPopHead()
