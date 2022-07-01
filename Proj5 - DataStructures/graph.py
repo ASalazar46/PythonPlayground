fbfc3cd456924e61f93bf20677adc734704593e0
ASalazar46 <andysal737@gmail.com>
2022-06-30 22:55:21 -0700

    Started on graphs

---
+++ Proj5 - DataStructures/graph.py
@@ -0,0 +1,16 @@
+# -----
+# -----
+# Graph
+#https://www.geeksforgeeks.org/python-data-structures/
+# -----
+# -----
+
+# Implementation of a graph with a 2d list / matrix
+class GraphViaMatrix:
+    def __init__(self):
+        pass
+
+# Implementation of a graph with a list
+class GraphViaList:
+    def __init__(self):
+        pass
