c278b80f25b9f772e5b3699d3f52077764625b2f
ASalazar46 <andysal737@gmail.com>
2022-07-02 23:07:33 -0700

    more graphs

--- Proj5 - DataStructures/graph.py
+++ Proj5 - DataStructures/graph.py
@@ -34,6 +34,32 @@ class Graph1:
                     edgeList.append({vertexValue, vertexKey})
         print(edgeList)
 
+# Directed, unweighted graph represented with a dictionary
+class Graph2:
+    def __init__(self, existGraph=None):
+        if existGraph == None:
+            self.adjDict = {}
+        else:
+            self.adjDict = existGraph
+    
+    def add_vertex(self, vertexKey):
+        if vertexKey not in self.adjDict:
+            self.adjDict[vertexKey] = []
+
+    def add_edge(self, fromVertexKey, toVertexKey):
+        if toVertexKey not in self.adjDict[fromVertexKey]:
+            self.adjDict[fromVertexKey].append(toVertexKey)
+
+    def print_vertices(self):
+        print(list(self.adjDict.keys()))
+
+    def print_edges(self):
+        edgeList = []
+        for vertexKey in self.adjDict:
+            for vertexValue in self.adjDict[vertexKey]:
+                    edgeList.append((vertexKey, vertexValue))
+        print(edgeList)
+
 testGraph = { 
    "a" : ["b","c"],
    "b" : ["a", "d"],
@@ -41,7 +67,7 @@ testGraph = {
    "d" : ["e"],
    "e" : ["d"]
 }
-
+'''
 graph = Graph1(testGraph)
 graph.print_vertices()
 graph.print_edges()
@@ -52,3 +78,14 @@ graph.add_edge('f', 'b')
 
 graph.print_vertices()
 graph.print_edges()
+'''
+graph2 = Graph2(testGraph)
+graph2.print_vertices()
+graph2.print_edges()
+
+graph2.add_vertex('f')
+graph2.add_edge('f', 'e')
+graph2.add_edge('f', 'b')
+
+graph2.print_vertices()
+graph2.print_edges()
