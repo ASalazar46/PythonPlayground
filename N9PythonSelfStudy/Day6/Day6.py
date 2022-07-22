3ba99ed433bb578b1648e40e9b5bdca238a5fec1
ASalazar46 <andysal737@gmail.com>
2022-07-22 12:34:09 -0700

    Day 6

--- N9PythonSelfStudy/Day6/Day6.py
+++ N9PythonSelfStudy/Day6/Day6.py
@@ -1,5 +1,6 @@
 # Day 6
 # File Operators
+from os import *
 
 # To work with files, we need to open a file stream
 # Use the open() function and give it a filename
@@ -8,6 +9,11 @@
 
 # Opened files must be closed
 '''file.close()'''
+
+# Closing a file writes data in the file stream to the
+# file and then closes the stream.
+# To keep the stream open, but write file stream data
+# to the file, use flush()
 
 # Using a 'with' statement automatically opens and closes the stream 
 # upon its code block's completion
@@ -22,3 +28,23 @@ with open('writeThisFile', 'w') as F:
     print('Write complete.')
 with open('writeThisFile', 'r') as F:
     print(F.read())
+
+# Append to a file
+with open('writeThisFile', 'a') as F:
+    F.write('I am an appended sentence.')
+with open('writeThisFile', 'r') as F:
+    print(F.read())
+
+# We can use the 'os' module to do more file operations
+
+# Make a new directory/folder
+'''mkdir('mkdirFolder')'''
+
+# Navigate to the directory
+'''chdir('mkdirFolder')'''
+
+# Rename a file
+'''rename('readThisFile', 'read')'''
+
+# Remove a file
+'''remove('read')'''
